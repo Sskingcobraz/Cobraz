60fcd1b1b7e35f45f1ffeb4719f06056fd90fa4e
Sskingcobraz <mrhellll99@gmail.com>
2025-07-08 21:04:15 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,24 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  signInWithPopup,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
-import { 
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
-
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyCQ3HiYLafcgfKDrVXzeP5tVGd3ephui5M",
   authDomain: "cobraz69.firebaseapp.com",
@@ -28,25 +8,10 @@ const firebaseConfig = {
   appId: "1:966165847096:web:7a131aae701d3a752b1acd"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-export { 
-  auth, 
-  provider, 
-  signInWithPopup, 
-  onAuthStateChanged,
-  signOut,
-  db,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs
-};
+// Global Firebase services
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.provider = new firebase.auth.GoogleAuthProvider();
