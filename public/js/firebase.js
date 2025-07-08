140e962c3134389d59db25b56f238680dca6b55e
Sskingcobraz <mrhellll99@gmail.com>
2025-07-08 20:23:06 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,20 +1,22 @@
-// Your web app's Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCQ3HiYLafcgfKDrVXzeP5tVGd3ephui5M",
-    authDomain: "cobraz69.firebaseapp.com",
-    projectId: "cobraz69",
-    storageBucket: "cobraz69.firebasestorage.app",
-    messagingSenderId: "966165847096",
-    appId: "1:966165847096:web:7a131aae701d3a752b1acd"
+  apiKey: "AIzaSyCQ3HiYLafcgfKDrVXzeP5tVGd3ephui5M",
+  authDomain: "cobraz69.firebaseapp.com",
+  projectId: "cobraz69",
+  storageBucket: "cobraz69.firebasestorage.app",
+  messagingSenderId: "966165847096",
+  appId: "1:966165847096:web:7a131aae701d3a752b1acd"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
-// Firestore settings
-db.settings({
-  merge: true
-});
+export { auth, provider, signInWithPopup };
